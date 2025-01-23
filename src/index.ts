331c5a8ad8f7be9906218bbe92d0331c1230b2cd
Vincent Petereit <vincent.petereit@greentwins.de>
2025-01-23 12:24:01 +0100

    Integration ThreeJS

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,8 @@ const projectsManager = new ProjectsManager(projectsListUI)
 
 // This document object is provided by the browser, and its main purpose is to help us interact with the DOM.
 const newProjectBtn = document.getElementById("new-project-btn")
-if (newProjectBtn) {
+
+if (newProjectBtn)  {
   newProjectBtn.addEventListener("click", () => {showModal("new-project-modal")})
 } else {
   console.warn("New projects button was not found")
